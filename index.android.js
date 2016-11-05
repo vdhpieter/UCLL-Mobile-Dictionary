ebdfef4a19332703447a9894f2342cab30fd5b4d
vdhpieter <pieter_vdh@live.be>
2016-11-05 09:11:21 +0100

    add text input

--- index.android.js
+++ index.android.js
@@ -13,19 +13,20 @@ import {
 } from 'react-native';
 
 export default class Dictionary extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {saerchText: ''};
+  }
+  
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TextInput
+        style={{height: 40}}
+        placeholder="Type here to lookup!"
+        onChangeText={(search) => this.setState({searchText})}
+      />
       </View>
     );
   }
@@ -38,16 +39,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  
 });
 
 AppRegistry.registerComponent('Dictionary', () => Dictionary);
